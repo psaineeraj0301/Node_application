78b7d14833da70e65cdfcb0b41f84d7f757a42cf
psaineeraj0301 <166980698+psaineeraj0301@users.noreply.github.com>
2024-07-25 17:45:36 +0530

    Update app.test.js

--- node_app/app.test.js
+++ node_app/app.test.js
@@ -1,12 +1,11 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('./app'); // Changed from '../app' to './app'
 
 describe('GET /', () => {
-  it('should return Hello World', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello World');
-  });
+    it('should return Hello World', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toBe('Hello World');
+    });
 });
-
 
