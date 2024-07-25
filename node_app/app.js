3dcd79f9099077720a6b27beeca1e0cb606ec84e
psaineeraj0301 <166980698+psaineeraj0301@users.noreply.github.com>
2024-07-25 17:28:24 +0530

    Update app.js

--- node_app/app.js
+++ node_app/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
+
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
 
